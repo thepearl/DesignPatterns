3eb81aa6e08bf7c41795b6e4430eedaaaa87ff76
Ghazi Tozri <ghazi.tozri98@gmail.com>
2020-12-18 22:53:15 +0100

    comment

--- src/Observer/MessageSubscriber_Three.java
+++
@@ -1,9 +0,0 @@
-package Observer;
-
-public class MessageSubscriber_Three implements Observer {
-    @Override
-    public void actualiser(MessageMeteo msg) {
-        System.out.println("Message Sub THREE :: HUMD " + msg.getHumidite() + " PRESS " + msg.getPression() + " TEMP " + msg.getTemerature());
-
-    }
-}
